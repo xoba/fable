18dcffeb7aeafa70334202d23de2741054834d3c
mike andrews <mra@xoba.com>
2026-06-19 12:32:12 -0400

    slow fable between lines

--- fable.sh
+++ fable.sh
@@ -4,7 +4,7 @@ d(){ perl -Mbytes -0777 -pe 'BEGIN{binmode STDIN;binmode STDOUT}$_^="\xA5"x leng
 T=cat;for a;do case $a in --help)d $D/00_*;exit;;--caps)T="tr a-z A-Z";;esac;done
 s(){ d $1|awk -v s=$(jot -r 1 1 999999) 'BEGIN{srand(s)}{print rand()"\t"$0}'|sort -n|cut -f2-;}
 A=$(s $D/01_*);B=$(s $D/02_*);C=$(s $D/03_*);R=$(s $D/04_*)
-t(){ z=$(jot -r 1 1 9);$T|fold -w1|while IFS= read -r c;do printf %s "$c";sleep 0.0$(jot -r 1 0 $z);done;echo;}
+t(){ z=$(jot -r 1 1 9);$T|fold -w1|while IFS= read -r c;do printf %s "$c";sleep 0.0$(jot -r 1 0 $z);done;echo;sleep 0.$(jot -r 1 3 9);}
 q(){ eval o=\$$1;h=${o%%'
 '*};v=${o#*
 };[ "$h" = "$o" ]||v="$v
